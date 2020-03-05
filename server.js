3ef2350887e36a63118016a4f1ddf409f88468be
Amanda Lima <54449491+amandalimaeng@users.noreply.github.com>
2020-03-04 22:13:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.get('*', (req,res) => res.sendFile('index.html' , { root : baseDir }))
 
 const port = 4000
 
-app.listen(port, () => console.log(`Servidor subiu com sucesso em http://localhost:${port}`))
+app.listen(process.env.PORT, () => console.log(`Servidor subiu com sucesso em http://localhost:${port}`))
